7f873de586c978680d154f18d86a2662f55f227e
RossiniM <rossini1408@gmail.com>
2022-04-16 17:38:18 -0300

    feat(validation): include validation at customer
    Validate if the entity is consistent whenever it is created.

--- src/customer.ts
+++ src/customer.ts
@@ -10,11 +10,22 @@
       this._id = id;
       this._address = address;
       this._name = name;
+      this.validate();
     }
 
 
     get id():string{
       return this._id;
+    }
+
+    validate(){
+      if(this._name.length === 0){
+        throw new Error("Name is required")
+      }
+
+      if(this._id.length === 0){
+        throw new Error("ID is required")
+      }
     }
 
     changeName(name: string){
