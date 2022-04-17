ffcacf4c22416a582fe03c52b34cc5669739e87e
RossiniM <rossini1408@gmail.com>
2022-04-17 11:46:11 -0300

    feat(produt): impl product aggregate and its tests

---
+++ src/entity/product.spec.ts
@@ -0,0 +1,64 @@
+
+import Product from "./product";
+
+describe("Product unit tests", () =>{
+
+  it("should throw err when id is empty", () => {
+
+    expect(() => {
+
+      const product = new Product("", "Product 1", 100 );
+
+    }).toThrowError("ID is required");
+  });
+
+  it("should throw err when name is empty", () => {
+
+    expect(() => {
+
+      const product = new Product("1", "", 100 );
+
+    }).toThrowError("Name is required");
+  });
+
+  it("should throw price when is invalid", () => {
+
+    expect(() => {
+
+      const product = new Product("1", "Product 1", - 100 );
+
+    }).toThrowError("Price is invalid");
+  });
+
+
+  it("should change name ", () => {
+
+      const product = new Product("1", "Product 1", 100 );
+      product.changeName("productName");
+      expect(product.name).toBe("productName");
+
+  });
+
+
+  it("should change price to valid value ", () => {
+
+    const product = new Product("1", "Product 1", 100 );
+    product.changePrice(200);
+    expect(product.price).toBe(200);
+
+});
+
+
+it("should change price to invalid value ", () => {
+
+ expect( ()=>{
+  const product = new Product("1", "Product 1", 100 );
+ 
+  product.changePrice(-5);
+
+ }).toThrowError("Price is invalid");
+
+
+});
+
+});
