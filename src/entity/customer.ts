99518ed7e96d0c05f7845409bb9ab945fd8ced88
RossiniM <rossini1408@gmail.com>
2022-04-21 14:36:54 -0300

    feat(customer): impl reward system

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -1,62 +1,71 @@
 import Address from "./adress"
 
-export default class Customer{
+export default class Customer {
 
-    private  _id: string;
-    private _name: string;
-    private _address: Address;
-    private _active: boolean = true;
+  private _id: string;
+  private _name: string;
+  private _address: Address;
+  private _rewardPoints: number = 0;
+  private _active: boolean = true;
 
-    constructor(id:string, name:string, address: Address){
+  constructor(id: string, name: string, address: Address) {
 
-      this._id = id;
-      this._address = address;
-      this._name = name;
-      this.validate();
+    this._id = id;
+    this._address = address;
+    this._name = name;
+    this.validate();
+  }
+
+
+  get id(): string {
+    return this._id;
+  }
+
+
+  get name(): string {
+    return this._name;
+  }
+
+  get rewardPoints(): number {
+    return this._rewardPoints;
+  }
+
+  addRewardPoints(points: number) {
+    this._rewardPoints += points;
+  }
+
+  validate() {
+    if (this._name.length === 0) {
+      throw new Error("Name is required")
     }
 
-
-    get id():string{
-      return this._id;
+    if (this._id.length === 0) {
+      throw new Error("ID is required")
     }
 
-
-    get name():string{
-      return this._name;
+    if (this._address == undefined) {
+      throw new Error("Address is required")
     }
+  }
 
-    validate(){
-      if(this._name.length === 0){
-        throw new Error("Name is required")
-      }
+  changeName(name: string) {
+    this._name = name;
+    this.validate();
+  }
 
-      if(this._id.length === 0){
-        throw new Error("ID is required")
-      }
+  activate() {
+    this._active = true;
+  }
 
-      if(this._address == undefined){
-        throw new Error("Address is required")
-      }
-    }
+  isActive(): boolean {
+    return this._active;
+  }
 
-    changeName(name: string){
-       this._name = name;
-       this.validate();
-    }
+  inactivate() {
+    this._active = false;
+  }
 
-    activate(){
-      this._active = true;
-    }
-
-    isActive(): boolean{
-      return this._active;
-    }
-
-    inactivate(){ 
-      this._active = false;
-    }
-
- }
+}
 /*
 
 Complexidade de negócio
--- src/services/order.service.spec.ts
+++ src/services/order.service.spec.ts
@@ -2,40 +2,34 @@ import { idText, isTemplateExpression } from "typescript"
 import OrderItem from "../entity/order_item"
 import Order from "../entity/order"
 import OrderService from "./order.service"
+import Customer from "../entity/customer"
+import Address from "../entity/adress"
 
 describe("Order service unit tests ", () => {
 
+  it("should sum total of all orders", () => {
 
+    const orderItem1 = new OrderItem("1", "Item 1", 100, "p1", 1)
+    const orderItem2 = new OrderItem("1", "Item 2", 200, "p2", 2)
 
-it("should sum total of all orders", () => {
+    const order1 = new Order("1", "c2", [orderItem1])
+    const order2 = new Order("2", "c2", [orderItem2])
 
-  const orderItem1 = new OrderItem("1","Item 1", 100, "p1", 1)
-  const orderItem2 = new OrderItem("1","Item 2", 200, "p2", 2)
+    expect(OrderService.getTotalOrder([order1, order2])).toBe(500);
 
-  const order1 = new Order("1","c2", [orderItem1])
-  const order2 = new Order("2","c2", [orderItem2])
+  })
 
-  expect(OrderService.getTotalOrder([order1, order2])).toBe(500);
+  it("should place an order and generate reward points", () => {
 
-})
+    const address = new Address("vereador nunes", 12, "88545788", "Aratuba")
+    const customer = new Customer("c1", "Joe", address);
+    const orderItem1 = new OrderItem("1", "Item 1", 10, "p1", 1)
 
+    const order = OrderService.placeOrder(customer, [orderItem1])
 
+    expect(customer.rewardPoints).toBe(5)
 
+    expect(order.total()).toBe(10)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  })
 })
