48c7049452dc21faa0a3c7bbaf20a6d13256ced9
RossiniM <rossini1408@gmail.com>
2022-04-21 15:06:58 -0300

    refactor: impl domain folder

---
+++ src/domain/repository/repository-interface.ts
@@ -0,0 +1,11 @@
+export default interface RepositoryInterface<T> {
+
+  create(entity: T): Promise<T>;
+  update(entity: T): Promise<void>;
+  find(id: string): Promise<T>;
+  findAll(): Promise<T[]>;
+  /*
+    incluir metadados de paginação
+  */
+
+}
