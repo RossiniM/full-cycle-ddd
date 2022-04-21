0521ca142744434ddd0d2c57ac9a2aab79167828
RossiniM <rossini1408@gmail.com>
2022-04-21 14:36:54 -0300

    feat(customer): impl reward system

--- src/services/order.service.ts
+++ src/services/order.service.ts
@@ -1,10 +1,22 @@
+import Customer from "../entity/customer";
 import Order from "../entity/order";
+import { v4 as uuid } from "uuid";
+import OrderItem from "../entity/order_item";
 
-export default class OrderService{
+export default class OrderService {
 
 
-static getTotalOrder(orderList:Order []): number{
-  return orderList.reduce((accumulator, order) => accumulator + order.total(),0);
-}
+  static getTotalOrder(orderList: Order[]): number {
+    return orderList.reduce((accumulator, order) => accumulator + order.total(), 0);
+  }
 
+  static placeOrder(customer: Customer, orderItemList: OrderItem[]): Order {
+    if (orderItemList.length === 0) {
+      throw new Error("Order must have at least one item")
+    }
+
+    const order = new Order(uuid(), customer.id, orderItemList);
+    customer.addRewardPoints(order.total() * 0.5);
+    return order;
+  }
 }
